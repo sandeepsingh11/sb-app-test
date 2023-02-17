f56ada2e58c5d1764915ef69ab2c6f9a5150d12d
Sandeep <sandeep@pop-os.localdomain>
2023-02-17 17:49:44 -0500

    init commit | basic setup

---
+++ src/routes/+page.server.ts
@@ -0,0 +1,14 @@
+import type { PageServerLoad } from "./$types";
+// import { LeannyService } from "$lib/LeannyService";
+
+export const load = (async (request) => {
+    // const ls = new LeannyService();
+    // const result = ls.calcSsu();
+
+    // const res = await request.fetch(`http://127.0.0.1:8000/api/test`);
+    // const res = await fetch(`http://127.0.0.1:8000/api/test`);
+    
+    // const data = await res.json();
+
+    // return {result};
+}) satisfies PageServerLoad;
